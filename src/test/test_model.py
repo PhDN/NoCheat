ab81939cc4fa5b2e1d3a590ffa8594e887ddadc4
PhDN <phuongnguyening@gmail.com>
2023-03-02 02:25:55 -0800

    fixed test_model.py linking issue, added argument parser to add test file input

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -1,22 +1,40 @@
-from src.back.model import GPT2PPL
+"""
+Purpose: Test the model's classification output on a given text file.
 
-# initialize the model
-model = GPT2PPL()
+usage: python3 test_model.py --test_file <file_name>.txt
+                             --device <(optional)'cuda' or 'cpu'>
 
-# print("Please enter your sentence: (Press Enter twice to start processing)")
-# contents = []
-# while True:
-#     line = input()
-#     if len(line) == 0:
-#         break
-#     contents.append(line)
-# sentence = "\n".join(contents)
+This script takes a text test file, 
+and output the model.GPT2PPL's classification result, 
+perplexity, perplixty per line, and burstiness score.
 
-# open text file in read mode
-text_file = open('data/test.txt', 'r')
-# read whole file to a string
-text = text_file.read()
-# close file
-text_file.close()
+This script optionally takes a device argument to run
+the model using cpu or gpu (cuda).
 
-model(text)
+Warning: The model takes ~1-2 minute to classify a document using cpu.
+""" 
+
+import argparse
+import sys
+sys.path.append('src/back/')
+from model import GPT2PPL
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test_file", type=str, required=True)
+    parser.add_argument("--device",
+                        type=str,
+                        choices=['cuda', 'cpu'],
+                        required=False)
+    args = parser.parse_args()
+
+    # initialize the model
+    model = GPT2PPL(device=args.device)
+    # open text file in read mode
+    text_file = open(args.test_file, 'r')
+    # read whole file to a string
+    text = text_file.read()
+    # close file
+    text_file.close()
+    # classify on given text file, prints output in shell
+    model(text)

c818aa756e4550af66a58740af7c1f4b837e85ee
Simon Struthers <snstruthers@gmail.com>
2023-03-05 14:46:48 -0800

    [DocumentPreview] Added more valid text formats +
    files that don't provide a type no longer get displayed.

--- src/front/src/DocumentPreview.js
+++ src/front/src/DocumentPreview.js
@@ -7,19 +7,33 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 /** @type {(document: Blob) => boolean} */
 const isPdf = document => ['application/pdf', 'application/x-pdf'].includes(document.type);
 
+const plainTextSet = new Set([
+    'bsh',
+    'csh',
+    'ecmascript',
+    'javascript',
+    'json',
+    'lisp',
+    'rtf',
+    'sh',
+    'tex',
+    'yaml',
+]);
+
 /** @type {(document: Blob) => boolean} */
-export const isPlainText = document => document.type.startsWith('text/')  ||
-    ['application/json', 'application/javascript', 'application/x-javascript'].includes(document.type);
+export const isPlainText = document => document.type.startsWith('text/') ||
+    /\+(?:xml|json)$/.test(document.type) ||
+    (document.type.startsWith('application/') &&
+        plainTextSet.has(document.type.match(/^application\/(?:x-)?(.+?)$/)[1]));
 
 /**
  * @param {{ document: Blob; }} props
  */
 export default function DocumentPreview({ document }) {
-    const [text, setText] = useState(
-        isPlainText(document) ? false : document.type);
+    const [text, setText] = useState(isPlainText(document) ? false : (document.type || 'unknown'));
 
     useEffect(() => {
-        if (!text) document.text().then(text => setText(text.slice(0, 2000)));
+        if (!text && isPlainText(document)) document.text().then(text => setText(text.slice(0, 2000)));
     }, [document, text]);
 
     return isPdf(document) ? <Document file={document} onLoadError={console.error.bind(console)}>

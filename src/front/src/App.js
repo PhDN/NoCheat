e5005c60751b5bb31b5bcf967219d062e5fd49b6
Muhammad <ammar.junejo0987@yahoo.com>
2023-03-06 01:33:09 -0800

    - added a logo - added description - added user instructions - changed color and text on the upload bar

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -21,8 +21,12 @@ export default function App() {
     return <DocumentStore.Provider value={useDocumentStore()}>
         <JobStore.Provider value={useJobsStore()}>
             <div className="App">
-                <nav>NoCheat</nav>
+                <nav>
+                <h1>NoCheat</h1>
+                </nav>
                 <main>
+                    <h2>Our powerful model can detect AI generated texts.</h2>
+                    <p>Check credibility of a text by uploading a file below or adding text to our editor using the add button on right.</p>
                     <DocumentsForm openEditModal={openEditModal} />
                     <JobsList />
                     {editing !== null && <EditModal id={editing} close={setEditing.bind(null, null)} />}
